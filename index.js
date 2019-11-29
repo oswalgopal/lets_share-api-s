292c3810a01844faa5a5b6f4374d6ceca9c589d0
Gopal Oswal <gopal@kritin.in>
2019-11-29 10:04:47 +0530

    fixed some api's

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-const express = require('express');
-const { Pool, Client } = require('pg');
-const app = express();
+const express = require('express');   /*** getting the express*/
+const { Pool, Client } = require('pg'); /*** getting the posgresql*/
+const app = express(); /*** creating a express app*/
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+/**
+ *  connecting to the database
+ */
 const pool = new Pool({
     user: 'postgres',
     host: 'localhost',
@@ -8,8 +14,16 @@ const pool = new Pool({
     password: '1234',
     port: 3000,
 });
-port = 5000;
+/**
+ * setting the port for the server
+ */
+port = 10000;
 
+vaccess_code = '';
+
+/**
+ * function to check the server root path
+ */
 app.get('/', (req,res) => {
     res.send({
         response: 'server is running on port' + port,
@@ -17,6 +31,24 @@ app.get('/', (req,res) => {
         message: 'Success'
     });
 });
+
+/**
+ * function to get the data using access_code
+ */
+
+app.get('/Admin?access_code=' + vaccess_code, (req,res) => {
+    pool.query('SELECT * from data where access_code = ' , vaccess_code , (err2, res2) => {
+        res.send({
+            response : res2.rows,
+            status: 200,
+            message: 'Success'
+        });
+    });
+});
+
+/**
+ * function to add the admin
+ */
 
 app.get('/addAdmin', (req,res) => {
     pool.query('SELECT * from addadmin()', (err2, res2) => {
@@ -27,6 +59,52 @@ app.get('/addAdmin', (req,res) => {
         });
     });
 });
+
+/**
+ * function to get the data
+ */
+app.get('/getData', (req,res) => {
+    pool.query(`select * from get_data(${req.body.access_code})`, (err2,res2) => {
+        console.log('error', err2);
+        console.log('res2', res2);
+        res.send({
+            response : res2,
+            status: 200,
+            message: 'Success'
+        })
+    })
+});
+
+
+/**
+ * function to add the data
+ */
+app.post('/add_data', (req,res) => {
+    // console.log(req.body);
+    console.log(typeof (req.body.access_code));
+    const query = `select * from add_data(${req.body.access_code}, \" ${req.body.data})`;
+    pool.query(query, (err2, res2) => {
+        console.log('error', err2);
+        // console.log('res2', res2);
+        if (err2) {
+            res.send({
+                response : err2,
+                status: 500,
+                message: 'Error'
+            });
+        } else {
+            res.send({
+                response : res2,
+                status: 200,
+                message: 'Success'
+            });
+        }
+    })
+});
+
+/**
+ * function to set the port of the server
+ */
 app.listen(port, () => {
     console.log('server is running on port' , port);
 });
